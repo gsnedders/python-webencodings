fe512c5b60fea06bb98cc8fe831fd889b383dc8f
Simon Sapin <simon.sapin@exyr.org>
2012-12-06 23:02:28 +0100

    Initial commit: LICENSE, README, and importable Python package.

---
+++ webencodings/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+    webencodings
+    ~~~~~~~~~~~~
+
+    This is a Python implementation of the `WHATWG Encoding standard
+    <http://encoding.spec.whatwg.org/>`. See README for details.
+
+    :copyright: Copyright 2012 by Simon Sapin
+    :license: BSD, see LICENSE for details.
+
+"""
