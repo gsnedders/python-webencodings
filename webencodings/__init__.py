16c81f9eb0f9a36e21e541a06a80d31936cb9858
Simon Sapin <simon.sapin@exyr.org>
2012-12-07 01:47:06 +0100

    Add {,utf8_}{encode,decode} and some tests.

--- webencodings/__init__.py
+++ webencodings/__init__.py
@@ -29,3 +29,44 @@ def lookup(label):
     # ASCII_WHITESPACE is Unicode, so the result of .strip() is Unicode.
     # We want the Unicode version of .translate().
     return LABELS[label.strip(ASCII_WHITESPACE).translate(ASCII_LOWERCASE_MAP)]
+
+
+def decode(byte_string, label, errors='replace'):
+    """
+    Decode a byte string with a fallback encoding given by its label.
+
+    **Note:** in accordance with the Encoding standard,
+    the default error handling for decode (but not for encode) is ``replace``,
+    ie. insert U+FFFD for invalid bytes.
+
+    """
+    # Throw on invalid label, even if there is a BOM.
+    encoding = lookup(label)
+    if byte_string.startswith((b'\xFF\xFE', b'\xFE\xFF')):
+        # Python’s utf_16 picks BE or LE based on the BOM.
+        encoding = 'utf_16'
+    elif byte_string.startswith(b'\xEF\xBB\xBF'):
+        # Python’s utf_8_sig skips the BOM.
+        encoding = 'utf_8_sig'
+    return byte_string.decode(encoding, errors)
+
+
+def encode(unicode_string, label, errors='strict'):
+    """Encode an Unicode string with an encoding given by its label."""
+    return unicode_string.encode(lookup(label), errors)
+
+
+# These two are here for completeness
+# and to give a nicer API than "Use utf_8_sig in, utf_8 out".
+
+def utf8_decode(byte_string, errors='strict'):
+    """Decode a byte string as UTF-8, skipping any BOM."""
+    # Python’s utf_8_sig skips a b'\xEF\xBB\xBF' BOM if there is one,
+    # is like utf_8 otherwise.
+    return byte_string.decode('utf_8_sig', errors)
+
+
+def utf8_encode(unicode_string, errors='strict'):
+    """Encode an Unicode string as UTF-8."""
+    # Encode without a BOM.
+    return unicode_string.encode('utf_8', errors)
--- webencodings/tests.py
+++ webencodings/tests.py
@@ -12,7 +12,7 @@
 """
 
 from __future__ import unicode_literals
-from . import lookup, LABELS
+from . import lookup, LABELS, decode, encode, utf8_decode, utf8_encode
 
 
 def do_lookup(label):
@@ -47,3 +47,43 @@ def test_names_are_valid_python():
     # TODO: implement x-user-defined
     for name in set(LABELS.values()) - set(['x-user-defined']):
         assert ''.encode(name) == b''
+
+
+def test_decode():
+    assert decode(b'\x80', 'latin1') == '€'
+    assert decode(b'\xc3\xa9', 'utf8') == 'é'
+    assert decode(b'\xc3\xa9', 'ascii') == 'Ã©'
+    assert decode(b'\xEF\xBB\xBF\xc3\xa9', 'ascii') == 'é'  # UTF-8 with BOM
+
+    assert decode(b'\xFE\xFF\x00\xe9', 'ascii') == 'é'  # UTF-16-BE with BOM
+    assert decode(b'\xFF\xFE\xe9\x00', 'ascii') == 'é'  # UTF-16-LE with BOM
+    assert decode(b'\xFE\xFF\xe9\x00', 'ascii') == '\ue900'
+    assert decode(b'\xFF\xFE\x00\xe9', 'ascii') == '\ue900'
+
+    assert decode(b'\x00\xe9', 'UTF-16BE') == 'é'
+    assert decode(b'\xe9\x00', 'UTF-16LE') == 'é'
+    assert decode(b'\xe9\x00', 'UTF-16') == 'é'
+
+    assert decode(b'\xe9\x00', 'UTF-16BE') == '\ue900'
+    assert decode(b'\x00\xe9', 'UTF-16LE') == '\ue900'
+    assert decode(b'\x00\xe9', 'UTF-16') == '\ue900'
+
+
+def test_encode():
+    assert encode('é', 'latin1') == b'\xe9'
+    assert encode('é', 'utf8') == b'\xc3\xa9'
+    assert encode('é', 'utf8') == b'\xc3\xa9'
+    assert encode('é', 'utf-16') == b'\xe9\x00'
+    assert encode('é', 'utf-16le') == b'\xe9\x00'
+    assert encode('é', 'utf-16be') == b'\x00\xe9'
+
+
+def test_utf8_decode():
+    # No BOM in the decoded string
+    assert utf8_decode(b'\xEF\xBB\xBF\xc3\xa9') == 'é'
+    assert utf8_decode(b'\xc3\xa9') == 'é'
+
+
+def test_utf8_encode():
+    # No BOM in the encoded string
+    assert utf8_encode('é') == b'\xc3\xa9'
