2234063868e1c7faeb1e614f3ba956efca9c6c92
Simon Sapin <simon.sapin@exyr.org>
2012-12-07 01:08:38 +0100

    Add setup.py and tox.ini files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webencodings',
+    url='https://github.com/SimonSapin/webencodings',
+    license='BSD',
+    author='Simon Sapin',
+    author_email='simon.sapin@exyr.org',
+    packages=find_packages(),
+)
